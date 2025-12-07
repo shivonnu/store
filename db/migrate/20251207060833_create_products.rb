f2e2a9fce0b09879f76461eb1b7e16317275a11d
shivonnu <shivonnu@gmail.com>
2025-12-07 15:30:07 +0900

    Productコントローラー作成、Homeから初期表示をProductへ変更

--- db/migrate/20251207060833_create_products.rb
+++ db/migrate/20251207060833_create_products.rb
@@ -2,6 +2,8 @@ class CreateProducts < ActiveRecord::Migration[8.0]
   def change
     create_table :products do |t|
       t.string :name
+      t.integer :price
+      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_13_063502) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_07_060833) do
   create_table "products", force: :cascade do |t|
     t.string "name"
+    t.integer "price"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
